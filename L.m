5f4856c937925728775c9cb26b5d2c5c071e5a77
marhes-lab <36937615+shakeebbb@users.noreply.github.com>
2018-10-29 20:53:05 -0600

    Added main.m L.m

---
+++ L.m
@@ -0,0 +1,42 @@
+function L = L(x)
+
+% Assumes single integrator model so that we have 3 states for a single
+% agent in the X vector and z is same and constant for all the quadrotors
+
+theta_1 = 60*pi/180;
+theta_2 = 90*pi/180;
+R = ( x(6)*sin(theta_1/2) );
+rho = ( x(6)*sin(theta_2/2) );
+e = exp(1);
+
+%a = -5*(R-rho);
+
+L = cvx(zeros(3));
+
+        L(1,2) = -min(1,pow_p(e, R - r(1,2,x)));
+        L(1,3) = -min(1,pow_p(e, R - r(1,3,x)));
+        L(1,1) = -L(1,2) - L(1,3);
+        
+        L(2,1) = L(1,2);
+        L(2,3) = 0;
+        L(2,2) = -L(2,1) - L(2,3);
+         
+        L(3,1) = L(1,3);
+        L(3,2) = L(2,3);
+        L(3,3) = -L(3,1) - L(3,2);
+         
+
+% L(1,2) = min(1,exp( R - r(1,2,x) )); %min(1,1*inv_pos(r(1,2,x))-1);
+% L(1,3) = min(1,exp( R - r(1,3,x) )); %min(1,exp(5*(r(1,3,x) - R) / (R - rho)));
+% L(1,2) = -L(1,2); L(1,3) = -L(1,3);
+% 
+% L(1,1) = -L(1,2) - L(1,3);
+% 
+% L(2,1) = L(1,2);
+% L(2,3) = 0;
+% L(2,2) = -L(2,1) - L(2,3);
+% 
+% L(3,1) = L(1,3);
+% L(3,2) = L(2,3);
+% L(3,3) = -L(3,1) - L(3,2);
+end
---
+++ main.m
@@ -0,0 +1,49 @@
+clc
+clear all
+close all
+
+%% ECE 649 - Convex Optimization Semester Project
+% Fall 2018
+
+%% 
+Ts = 0.1; % Sampling Time
+Th = 10; % Time horizon
+N = Th/Ts; % Number of the waypoints
+n = 2; % Number of quadrotors
+l = 1; % Number of ground robots
+
+% x = [0 0 0 0.0 0 1 0. 0 1]';
+% L = L(x)
+% eig(L)
+% 
+P = [-1    -1    0;
+    1    0     -1;
+    0     1     1];
+
+leader_pose = [0;2;0];  % x,y,z coordinates of the ground robot
+
+cvx_begin sdp
+    %variable x(9)
+    variable lambda
+    variables x(9)
+    %variable Lm(3,3) symmetric
+    maximize( lambda )
+    subject to
+        %norm(chol(L(x))*P) >= lambda * eye(2)
+        %P' * Lm * P >= lambda * eye(3)
+        
+        P' * L(x) * P >= lambda * eye(3)
+        
+%         Lm(1,2) == -min(1,pow_p(e, R - r(1,2,x)));
+%         Lm(1,3) == -min(1,pow_p(e, R - r(1,3,x)));
+%         Lm(1,1) == -Lm(1,2) - Lm(1,3)
+%          
+%         Lm(2,2) == -Lm(2,1) - Lm(2,3)
+%          
+%         Lm(3,3) == -Lm(3,1) - Lm(3,2)
+%          
+%         Lm(2,3) == 0
+        
+        x(1:3) == leader_pose;
+cvx_end
+x
