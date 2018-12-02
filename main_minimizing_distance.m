ff49b6ad4bb84c36963fd8425a5d1988c6370fc3
marhes-lab <36937615+shakeebbb@users.noreply.github.com>
2018-12-02 15:06:17 -0700

    Working minimize distance with the MILP obstacle avoidance in simulations - Starting doing experiments

--- main_minimizing_distance.m
+++ main_minimizing_distance.m
@@ -20,7 +20,7 @@ l = 1; % Number of ground robots
 obs_loc = [1;2;1]; % Static obstacle location
 obs_width = 0.25; % Obstacle Width / 2
 obs_len = 0.25; % Obstacle Length / 2
-M = 1000; % Big M
+M = 10; % Big M
 
 
 % x = [0 0 0 0.0 0 1 0. 0 1]';
@@ -41,28 +41,29 @@ An = blkdiag(AnCell{:});
 Bn = blkdiag(BnCell{:});
 
 % Double Integrator Discrete Model for nRobots for N Waypoints
-ANCell = repmat({An}, 1, N-1);
-BNCell = repmat({Bn}, 1, N-1);
-AN = blkdiag(ANCell{:});
-BN = blkdiag(BNCell{:});
+% ANCell = repmat({An}, 1, N-1);
+% BNCell = repmat({Bn}, 1, N-1);
+% AN = blkdiag(ANCell{:});
+% BN = blkdiag(BNCell{:});
 
 % Mixed Integer Constraints
-Am = [zeros(6,4);
-      1  0 -1 0;
-      0 -1  0 1;
-      0  0  0 0;
-      zeros(3,4);
-      zeros(3,4);
-      zeros(3,4);
-      ]';
-% Am = [1  0 -1 0;
+% Am = [zeros(6,4);
+%       1  0 -1 0;
 %       0 -1  0 1;
-%       0  0  0 0;];
-bm = [obs_loc(1)+obs_len;
-      obs_loc(2)-obs_len;
-      obs_loc(1)-obs_len;
-      obs_loc(2)+obs_len;
-      ];
+%       0  0  0 0;
+%       zeros(3,4);
+%       zeros(3,4);
+%       zeros(3,4);
+%       ]';
+Am = [0  1  0  -1;
+      1  0  -1 0;
+      0  0  0  0;]';
+b_2 = obs_loc(1)+obs_len;
+b_3 = obs_loc(2)-obs_len;
+b_4 = obs_loc(1)-obs_len;
+b_1 = obs_loc(2)+obs_len;
+
+bm = [b_1 b_2 -b_3 -b_4]';
 
 AMCell = repmat({Am}, 1, N);
 
@@ -79,53 +80,92 @@ P = [-1    -1    0;
 % 
 % [sys_d] = c2d(ss(A,B,[],[]), Ts);
 
-leader_pose = [3 15 0 0 0 0]';  % Leader's State
+leader_pose = [1 2 0 0 0 0]';  % Leader's State
 x_init = [leader_pose' 0.0 0 2 0 0 0 0.0 0 2 0 0 0]';
-x_init(7:12) =  [0.6438 1.1557 0 0 0 0]';
+ x_init(7:12) =  [0.6438 1.1557 0 0 0 0]';
+%x_init(7:12) =  [0 1 0 0 0 0]';
 %% Solving Convex Optimization Problem with Dynamic Constraints using 'cvx'
 
-cvx_begin sdp
+cvx_begin
     %variable x(9)
-    variable lambda1
-    variable lambda2
-    variable lambda3
-    variable X(nStates*(N),1)
-    variable U(nInputs*(N-1),1)
-    variable wm(4*N)
+    variable X(nStates,N)
+    variable U(nInputs,N-1)
+    variable wm(4,N) binary
+    variable lambda
+    variable ux_max
+    variable uy_max
     %variable Lm(3,3) symmetric
-    maximize( lambda1+lambda2+lambda3 )
+    minimize( lambda + 0.3*ux_max + 0.3*uy_max)
     subject to
+       % for i = N-1 
+%        i = N-3;
+%         r(1,2,X((i-1)*nStates+1:i*nStates),nStatesPerRobot) <= lambda(i)
+       % end
         
-        P' * L(X(end-nStates+1:end), nStatesPerRobot) * P >= lambda1 * eye(3) % Network Strength Constraints
-        P' * L(X(end-2*nStates+1:end-nStates), nStatesPerRobot) * P >= lambda2 * eye(3) % Network Strength Constraints
-        P' * L(X(end-3*nStates+1:end-2*nStates), nStatesPerRobot) * P >= lambda3 * eye(3)
-         
-        X(nStates+1:end) == AN*X(1:end-nStates) + BN*U % Dynamic Constraints
+        r(1,2,X(:,N),nStatesPerRobot) <= lambda
+    
+        %r(1,2,X(end-2*nStates+1:end-nStates),nStatesPerRobot) <= lambda(2)
+        %r(1,2,X(end-3*nStates+1:end-2*nStates),nStatesPerRobot) <= lambda(3)
         
-        eye(nInputs*(N-1))*U <= 4*ones(nInputs*(N-1),1)
-        eye(nInputs*(N-1))*U >= -4*ones(nInputs*(N-1),1)
+        X(:,2:N) == An*X(:,1:N-1) + Bn*U(:,1:N-1) % Dynamic Constraints
+        
+        %eye(nInputs*(N-1))*U <= 4*ones(nInputs*(N-1),1)
+        %eye(nInputs*(N-1))*U >= -4*ones(nInputs*(N-1),1)
         %U(9) >= -5
         
         %X(end-nStates+1 : end-nStates+nStatesPerRobot) == leader_pose % Leader Final Conditions
-        U([1:nInputs:end]) == zeros(N-1,1)
-        U([2:nInputs:end]) == zeros(N-1,1)
-        U([3:nInputs:end]) == zeros(N-1,1)
-        U([6:nInputs:end]) == zeros(N-1,1)
-        U([9:nInputs:end]) == zeros(N-1,1)
-        X(1:nStates) == x_init
+        U(1,:) == zeros(1,N-1)
+        U(2,:) == zeros(1,N-1)
+        U(3,:) == zeros(1,N-1)
+        U(6,:) == zeros(1,N-1)
+        U(9,:) == zeros(1,N-1)
+%         
+         norm(U(4,:)) <= ux_max
+         norm(U(5,:)) <= uy_max
+
+%         U(4,:) <= 10*ones(1,N-1)
+%         U(5,:) <= 10*ones(1,N-1)
+%         
+%         U(4,:) >= -10*ones(1,N-1)
+%         U(5,:) >= -10*ones(1,N-1)
+        
+        
+%         U(7,:) <= 30*ones(1,N-1)
+%         U(8,:) <= 30*ones(1,N-1)
+%         
+%         U(4,:) >= -30*ones(1,N-1)
+%         U(5,:) >= -30*ones(1,N-1)
+%         U(7,:) >= -30*ones(1,N-1)
+%         U(8,:) >= -30*ones(1,N-1)
+
+        X(:,1) == x_init
        % X(nStatesPerRobot+1:nStatesPerRobot+3) == x_init(nStatesPerRobot+1:nStatesPerRobot+3)
        % X(2*nStatesPerRobot+1:2*nStatesPerRobot+3) == x_init(2*nStatesPerRobot+1:2*nStatesPerRobot+3)
         % Initial Conditions
-        
+%         
 %         Am = [1  0 -1 0
 %               0 -1  0 1
 %               0  0  0 0]'; % For each state TODO : Extend to X vector 
-%         bm = [obs_loc(1)+obs_len obs_loc(2)-obs_len obs_loc(1)-obs_len obs_loc(2)+obs_len]';
-        ones(4*N,1)' * wm <= 4-1
-        ones(4*N,1)' * wm >= 0
-        wm <= ones(4*N,1)
-        wm >= zeros(4*N,1)
-        AM * X + M*wm >= BM
+        %bm = [obs_loc(1)+obs_len obs_loc(2)-obs_len obs_loc(1)-obs_len obs_loc(2)+obs_len]';
+        
+        %%ones(4*N,1)' * wm <= 4-1
+        %ones(4*N,1)' * wm >= 0
+        %wm <= ones(4*N,1)
+        %wm >= zeros(4*N,1)
+        %%AM * X + M*wm >= BM
+% %         
+        Am*X(nStatesPerRobot+1:nStatesPerRobot+3,:) + M*wm >= [bm(1)*ones(1,N); bm(2)*ones(1,N); bm(3)*ones(1,N); bm(4)*ones(1,N)]
+        
+%         Am(1,:) * X(nStatesPerRobot+1:nStatesPerRobot+3,:) + M*wm(1,:) >= bm(1)*ones(1,N)
+%         Am(2,:) * X(nStatesPerRobot+1:nStatesPerRobot+3,:) + M*wm(2,:) >= bm(2)*ones(1,N)
+%         Am(3,:) * X(nStatesPerRobot+1:nStatesPerRobot+3,:) + M*wm(3,:) >= bm(3)*ones(1,N)
+%         Am(4,:) * X(nStatesPerRobot+1:nStatesPerRobot+3,:) + M*wm(4,:) >= bm(4)*ones(1,N)
+        
+        ones(1,4) * wm <= 3*ones(1,N)
+        %ones(1,4) * wm >= zeros(1,N)
+        
+        %Am * X(4:6,N-1) + M*wm(:,1) >= bm
+        %ones(1,4)*wm(:,1)  <= 3
         
         %Am*X(end-nStates+nStatesPerRobot+1 : end-nStates+2*nStatesPerRobot) + M*wm >= bm;
        %Am*X(end-nStatesPerRobot+1:end-nStatesPerRobot+3) + M*wm >= bm;
@@ -141,10 +181,10 @@ close all
 
 t = 0:Ts:Th;
 subplot(2,2,1);
-plot(t, X(1:nStates:end))
+plot(t, X(1,:));
 hold on
-plot(t, X(2:nStates:end))
-plot(t, X(3:nStates:end))
+plot(t, X(2,:));
+plot(t, X(3,:));
 hold off
 xlabel('Ground Robot (Leader)');
 ylabel('Position (m)');
@@ -152,20 +192,20 @@ legend('x','y','z');
 grid on
 
 subplot(2,2,2);
-plot(t, X(nStatesPerRobot+1:nStates:end))
+plot(t, X(nStatesPerRobot+1,:))
 hold on
-plot(t, X(nStatesPerRobot+2:nStates:end))
-plot(t, X(nStatesPerRobot+3:nStates:end))
+plot(t, X(nStatesPerRobot+2,:))
+plot(t, X(nStatesPerRobot+3,:))
 hold off
 xlabel('Aerial Robot 1 (Follower)');
 ylabel('Position (m)');
 legend('x','y','z');
 grid on
 subplot(2,2,3);
-plot(t, X(2*nStatesPerRobot+1:nStates:end))
+plot(t, X(2*nStatesPerRobot+1,:))
 hold on
-plot(t, X(2*nStatesPerRobot+2:nStates:end))
-plot(t, X(2*nStatesPerRobot+3:nStates:end))
+plot(t, X(2*nStatesPerRobot+2,:))
+plot(t, X(2*nStatesPerRobot+3,:))
 hold off
 xlabel('Aerial Robot 2 (Follower)');
 ylabel('Position (m)');
@@ -176,7 +216,7 @@ title('Leaders Position = [1,2,0]')
 %Trajectories plot on Euclidean Coordinates
 figure
 
-scatter3(X(1:nStates:end), X(2:nStates:end), X(3:nStates:end));
+scatter3(X(1,:), X(2,:), X(3,:));
 xlabel('x (m)');
 ylabel('y (m)');
 zlabel('z (m)');
@@ -186,9 +226,9 @@ zlim([0 3]);
 
 grid on; hold on;
 
-scatter3(X(nStatesPerRobot+1:nStates:end), X(nStatesPerRobot+2:nStates:end), X(nStatesPerRobot+3:nStates:end));
+scatter3(X(nStatesPerRobot+1,:), X(nStatesPerRobot+2,:), X(nStatesPerRobot+3,:));
 
-scatter3(X(2*nStatesPerRobot+1:nStates:end), X(2*nStatesPerRobot+2:nStates:end), X(2*nStatesPerRobot+3:nStates:end));
+scatter3(X(2*nStatesPerRobot+1,:), X(2*nStatesPerRobot+2,:), X(2*nStatesPerRobot+3,:));
 
 legend('Ground Robot (Leader)', 'Aerial Robot 1', 'Aerial Robot 2');
 
